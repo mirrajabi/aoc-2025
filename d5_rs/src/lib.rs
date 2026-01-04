6baa86e0ba5e175510f3f61d36fe74c731b1d7c2
Mad Mirrajabi <mohammadmirrajabi@gmail.com>
2026-01-04 01:05:20 +0100

    Day 5 part 2 in Rust

--- d5_rs/src/lib.rs
+++ d5_rs/src/lib.rs
@@ -3,6 +3,68 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+fn merge_ranges(ranges: Vec<(u64, u64)>) -> Vec<(u64, u64)> {
+    let mut starts = ranges.iter().map(|f| f.0).collect::<Vec<u64>>();
+    let starts = &mut starts[..];
+    starts.sort_unstable();
+    let mut ends = ranges.iter().map(|f| f.1).collect::<Vec<u64>>();
+    let ends = &mut ends[..];
+    ends.sort_unstable();
+
+    let len_ranges = ranges.len();
+
+    // Vec has more capacity allocated than necessary but it's fine in this case. We are sacrificing a bit of
+    // memory for performance juice
+    let mut ranges = Vec::with_capacity(len_ranges);
+    let mut si = 0;
+    loop {
+        if si >= len_ranges {
+            break;
+        }
+
+        let mut pi = 0;
+        let siv = starts[si];
+
+        for i in 1..(len_ranges - si) {
+            println!(
+                "Comparing start {} with end {}",
+                starts[si + i],
+                ends[si + pi]
+            );
+            if starts[si + i] > ends[si + pi] {
+                println!(
+                    "  No more overlap found: start {} > end {}",
+                    starts[si + i],
+                    ends[si + pi]
+                );
+                break;
+            }
+            println!(
+                "  Overlap found: start {} <= end {}",
+                starts[si + i],
+                ends[si + pi]
+            );
+            pi += 1;
+        }
+
+        ranges.push((siv, ends[si + pi]));
+        si += if pi > 0 { pi + 1 } else { 1 };
+    }
+
+    ranges
+}
+
+///
+/// The idea is to just sort start and end and values and count how many starts <= x <= end
+/// We then substract the index of the last start we found from the index of the last end we found, like so:
+/// let num_ranges = (last_start_idx as i32) - (last_end_idx as i32);
+///
+/// If num_ranges is negative or zero, that means that the last range-end that we found had an index higher than the
+/// last range-start that we found. Basically the value is outside of any range.
+///
+/// If the num_ranges is a positive integer, then its value equals to how many ranges contain this value!
+/// We are of course skipping that because the challenge doesn't require it but it was a nice learning experience on how
+/// to achieve that specific task too.
 fn count_values_within_ranges(ranges: Vec<(u64, u64)>, values: Vec<u64>) -> u16 {
     let mut starts = ranges.iter().map(|f| f.0).collect::<Vec<u64>>();
     let starts = &mut starts[..];
@@ -104,13 +166,24 @@ mod tests {
         println!("Values: {:?}", &values);
         println!();
 
+        let cloned_ranges = ranges.clone();
+
         let total = count_values_within_ranges(ranges, values);
         println!("Total: {}", total);
         assert_eq!(3, total);
+
+        // Challenge 2
+
+        let merged_ranges = merge_ranges(cloned_ranges);
+        let sum_ranges = merged_ranges
+            .iter()
+            .fold(0, |acc, elem| acc + (elem.1 - elem.0) + 1);
+        println!("Merged ranges: {:?}", merged_ranges);
+        println!("Sum of ranges: {}", sum_ranges);
+        assert_eq!(14, sum_ranges);
     }
 
-    // $ time cargo test
-    // cargo test  0.07s user 0.03s system 89% cpu 0.111 total
+    // The release binary will run this in less than 1ms with a warm cache
     #[test]
     fn test_personal_case() {
         let file = File::open("./assets/personal.txt").unwrap();
@@ -120,7 +193,15 @@ mod tests {
         println!();
         println!("Values: {:?}", &values);
 
-        let total = count_values_within_ranges(ranges, values);
+        let merged_ranges = merge_ranges(ranges.clone());
+        let sum_ranges = merged_ranges
+            .iter()
+            .fold(0, |acc, elem| acc + (elem.1 - elem.0) + 1);
+        println!("Merged ranges: {:?}", merged_ranges);
+        println!("Sum of ranges: {}", sum_ranges);
+        assert_eq!(344378119285354, sum_ranges);
+
+        let total = count_values_within_ranges(merged_ranges, values);
         assert_eq!(888u16, total);
     }
 }
