128d939c36a59241fa7a8bc9c7f27aa187edbbda
Mad Mirrajabi <mohammadmirrajabi@gmail.com>
2026-01-04 20:23:05 +0100

    Day 6 part 2 in Elixir

--- d6_ex/lib/lib.ex
+++ d6_ex/lib/lib.ex
@@ -9,6 +9,16 @@ defmodule Lib do
     |> Enum.sum()
   end
 
+  def calculate_sum_vertically(text) do
+    text
+    |> String.split("\n")
+    # The space at the end is a hack to include the final batch in the sum
+    |> Enum.map(fn str -> String.graphemes(str) ++ [" "] end)
+    |> Enum.zip()
+    |> Enum.reduce(%{sign: nil, sum: 0, batch: -1}, &accumulate_vertically/2)
+    |> Map.get(:sum)
+  end
+
   def split_line(line) do
     line
     |> String.split(" ")
@@ -19,10 +29,38 @@ defmodule Lib do
     col = Tuple.to_list(col)
     len_col = length(col)
     sign = Enum.at(col, -1)
-    nums = col |> Enum.take(len_col - 1) |> Enum.map(&String.to_integer/1)
+
+    nums =
+      col
+      |> Enum.take(len_col - 1)
+      |> Enum.map(&String.to_integer/1)
 
     op = if sign == "+", do: fn acc, num -> acc + num end, else: fn acc, num -> acc * num end
 
     nums |> Enum.reduce(op)
+  end
+
+  def accumulate_vertically(col, acc) do
+    col = Tuple.to_list(col)
+
+    if Enum.all?(col, fn x -> x == " " end) do
+      %{acc | batch: -1, sum: acc.sum + acc.batch}
+    else
+      len_col = length(col)
+      sign = Enum.at(col, -1)
+      sign = if sign == " ", do: acc.sign, else: sign
+
+      n =
+        col
+        |> Enum.take(len_col - 1)
+        |> List.to_string()
+        |> String.trim()
+        |> String.to_integer()
+
+      op = if sign == "+", do: fn acc, num -> acc + num end, else: fn acc, num -> acc * num end
+
+      batch = if acc.batch == -1, do: n, else: op.(acc.batch, n)
+      %{acc | batch: batch, sign: sign}
+    end
   end
 end
--- d6_ex/test/lib_test.exs
+++ d6_ex/test/lib_test.exs
@@ -24,4 +24,21 @@ defmodule LibTest do
 
     assert out == 4_309_240_495_780
   end
+
+  test "sample case pt2" do
+    out =
+      File.read!("./priv/sample.txt")
+      |> Lib.calculate_sum_vertically()
+
+    assert out == 3_263_827
+  end
+
+  test "personal case pt2" do
+    # Total # of windows = len - count + 1
+    out =
+      File.read!("./priv/input.txt")
+      |> Lib.calculate_sum_vertically()
+
+    assert out == 9_170_286_552_289
+  end
 end
